2cc4775553fd026b813928dc89c7fdc62792cec1
Seijiro Ozawa <seijiro4751@gmail.com>
2018-11-11 17:47:41 +0900

    impl key input

--- inputs/game.js
+++ inputs/game.js
@@ -1,3 +1,50 @@
+function keyboard(value) {
+  let key = {};
+  key.value = value;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = event => {
+    if (event.key === key.value) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+      event.preventDefault();
+    }
+  };
+
+  //The `upHandler`
+  key.upHandler = event => {
+    if (event.key === key.value) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+      event.preventDefault();
+    }
+  };
+
+  //Attach event listeners
+  const downListener = key.downHandler.bind(key);
+  const upListener = key.upHandler.bind(key);
+
+  window.addEventListener(
+    "keydown", downListener, false
+  );
+  window.addEventListener(
+    "keyup", upListener, false
+  );
+
+  // Detach event listeners
+  key.unsubscribe = () => {
+    window.removeEventListener("keydown", downListener);
+    window.removeEventListener("keyup", upListener);
+  };
+
+  return key;
+}
+
 var app = new PIXI.Application({ width: 800, height: 600 });
 app.renderer.backgroundColor = 0xFF00FF;
 app.renderer.autoResize = true;
@@ -11,7 +58,6 @@ PIXI.loader
 
 //Define any variables that are used in more than one function
 let cat, state;
-
 function setup() {
 
   //Create the `cat` sprite
@@ -20,6 +66,62 @@ function setup() {
   cat.vx = 0;
   cat.vy = 0;
   app.stage.addChild(cat);
+
+  //Capture the keyboard arrow keys
+  let left = keyboard("ArrowLeft"),
+      up = keyboard("ArrowUp"),
+      right = keyboard("ArrowRight"),
+      down = keyboard("ArrowDown");
+
+  //Left arrow key `press` method
+  left.press = () => {
+    //Change the cat's velocity when the key is pressed
+    cat.vx = -5;
+    cat.vy = 0;
+  };
+
+  //Left arrow key `release` method
+  left.release = () => {
+    //If the left arrow has been released, and the right arrow isn't down,
+    //and the cat isn't moving vertically:
+    //Stop the cat
+    if (!right.isDown && cat.vy === 0) {
+      cat.vx = 0;
+    }
+  };
+
+  //Up
+  up.press = () => {
+    cat.vy = -5;
+    cat.vx = 0;
+  };
+  up.release = () => {
+    if (!down.isDown && cat.vx === 0) {
+      cat.vy = 0;
+    }
+  };
+
+  //Right
+  right.press = () => {
+    cat.vx = 5;
+    cat.vy = 0;
+  };
+  right.release = () => {
+    if (!left.isDown && cat.vy === 0) {
+      cat.vx = 0;
+    }
+  };
+
+  //Down
+  down.press = () => {
+    cat.vy = 5;
+    cat.vx = 0;
+  };
+  down.release = () => {
+    if (!up.isDown && cat.vx === 0) {
+      cat.vy = 0;
+    }
+  };
 
   //Set the game state
   state = play;
@@ -36,7 +138,7 @@ function gameLoop(delta){
 
 function play(delta) {
 
-  //Move the cat 1 pixel to the right each frame
-  cat.vx = 1
+  //Use the cat's velocity to make it move
   cat.x += cat.vx;
+  cat.y += cat.vy
 }
